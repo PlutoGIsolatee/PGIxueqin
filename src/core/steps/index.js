c1fef7c00b9228e16c93f6ef0c6a67d3cf042ec6
PlutoGIsolatee <plutoqweguo@126.com>
2026-07-30 19:51:17 +0800

    清理

--- src/core/steps/index.js
+++
@@ -1,7 +0,0 @@
-export { stepMahalanobisWindowDetection } from './stepMahalanobisWindowDetection.js';
-export { stepMahalanobisRefine } from './stepMahalanobisRefine.js';
-export { stepConsecutiveDigits } from './stepConsecutiveDigits.js';
-export { stepLowEntropyParagraph } from './stepLowEntropyParagraph.js';
-export { stepNormalParagraphExemption } from './stepNormalParagraphExemption.js';
-export { stepTrimWhitespace } from './stepTrimWhitespace.js';
-export { stepMergeIntervals } from './stepMergeIntervals.js';
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { CharCatConfig } from "./charCatConfig.js";
-import { TextProcess } from "./textProcess.js";
-import { Calculate } from "./calculate.js";
-import { charFreqLayers, charFreqLayersMani } from "./charFreqCats.js";
-import { Analyze } from './analyze.js';
-import { CharFreqModel } from './charFreqModel.js';
